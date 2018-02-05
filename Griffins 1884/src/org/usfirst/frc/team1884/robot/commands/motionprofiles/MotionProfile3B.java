7397d95771316afa689d49dc556b11c9e8354e75
Axel <axel_henrikson@asl.org>
2018-02-05 18:50:42 +0000

    Auto code

--- Griffins 1884/src/org/usfirst/frc/team1884/robot/commands/motionprofiles/MotionProfile3B.java
+++ Griffins 1884/src/org/usfirst/frc/team1884/robot/commands/motionprofiles/MotionProfile3B.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team1884.robot.commands.motionprofiles;
 
+import org.usfirst.frc.team1884.robot.Robot;
+import org.usfirst.frc.team1884.robot.subsystems.Drivetrain;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -10,27 +13,34 @@ public class MotionProfile3B extends Command {
     public MotionProfile3B() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+		requires(Robot.drivetrain);
+
     }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+ // Called just before this Command runs the first time
+ 	protected void initialize() {
+ 		Robot.drivetrain.initializeMotionProfile(Drivetrain.LeftMotionProfiles.left3B,
+ 				Drivetrain.RightMotionProfiles.right3B);
+ 		Robot.drivetrain.enableMotionProfile();
+ 	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+ 	// Called repeatedly when this Command is scheduled to run
+ 	protected void execute() {
+ 	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+ 	// Make this return true when this Command no longer needs to run execute()
+ 	protected boolean isFinished() {
+ 		return Robot.drivetrain.motionProfileIsDone();
+ 	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+ 	// Called once after isFinished returns true
+ 	protected void end() {
+ 		Robot.drivetrain.disableMotionProfile();
+ 	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+ 	// Called when another command which requires one or more of the same
+ 	// subsystems is scheduled to run
+ 	protected void interrupted() {
+ 		end();
+ 	}
 }
