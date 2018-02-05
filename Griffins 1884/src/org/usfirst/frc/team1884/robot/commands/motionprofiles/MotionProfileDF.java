ff4cc8fb4c41b1a20daf9887d057501207fd606a
Stella <stella_jorgensen@asl.org>
2018-02-05 18:50:31 +0000

    did stuff

--- Griffins 1884/src/org/usfirst/frc/team1884/robot/commands/motionprofiles/MotionProfileDF.java
+++ Griffins 1884/src/org/usfirst/frc/team1884/robot/commands/motionprofiles/MotionProfileDF.java
@@ -1,5 +1,8 @@
 package org.usfirst.frc.team1884.robot.commands.motionprofiles;
 
+import org.usfirst.frc.team1884.robot.Robot;
+import org.usfirst.frc.team1884.robot.subsystems.Drivetrain;
+
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
@@ -10,10 +13,14 @@ public class MotionProfileDF extends Command {
     public MotionProfileDF() {
         // Use requires() here to declare subsystem dependencies
         // eg. requires(chassis);
+		requires(Robot.drivetrain);
     }
 
     // Called just before this Command runs the first time
     protected void initialize() {
+		Robot.drivetrain.initializeMotionProfile(Drivetrain.LeftMotionProfiles.left1A,
+		Drivetrain.RightMotionProfiles.right1A);
+		Robot.drivetrain.enableMotionProfile();
     }
 
     // Called repeatedly when this Command is scheduled to run
@@ -22,15 +29,17 @@ public class MotionProfileDF extends Command {
 
     // Make this return true when this Command no longer needs to run execute()
     protected boolean isFinished() {
-        return false;
+		return Robot.drivetrain.motionProfileIsDone();
     }
 
     // Called once after isFinished returns true
     protected void end() {
+		Robot.drivetrain.disableMotionProfile();
     }
 
     // Called when another command which requires one or more of the same
     // subsystems is scheduled to run
     protected void interrupted() {
+    		end(); 
     }
 }
