395616b2a4bd2bbca504807867110815617e86d3
Kaito Arai <kaito_arai@asl.org>
2018-02-05 18:41:47 +0000

    Example motion profile command

--- Griffins 1884/src/org/usfirst/frc/team1884/robot/commands/motionprofiles/MotionProfile1A.java
+++ Griffins 1884/src/org/usfirst/frc/team1884/robot/commands/motionprofiles/MotionProfile1A.java
@@ -1,39 +1,45 @@
 package org.usfirst.frc.team1884.robot.commands.motionprofiles;
 
 import org.usfirst.frc.team1884.robot.Robot;
+import org.usfirst.frc.team1884.robot.subsystems.Drivetrain;
 
 import edu.wpi.first.wpilibj.command.Command;
 
 /**
- * drives robot straight from left starting position 
+ * drives robot straight from left starting position
  */
 public class MotionProfile1A extends Command {
-	
-    public MotionProfile1A() {
-        // Use requires() here to declare subsystem dependencies
-        // eg. requires(chassis);
-    		requires(Robot.drivetrain); 
-    }
 
-    // Called just before this Command runs the first time
-    protected void initialize() {
-    }
+	public MotionProfile1A() {
+		// Use requires() here to declare subsystem dependencies
+		// eg. requires(chassis);
+		requires(Robot.drivetrain);
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    protected void execute() {
-    }
+	// Called just before this Command runs the first time
+	protected void initialize() {
+		Robot.drivetrain.initializeMotionProfile(Drivetrain.LeftMotionProfiles.left1A,
+				Drivetrain.RightMotionProfiles.right1A);
+		Robot.drivetrain.enableMotionProfile();
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    protected boolean isFinished() {
-        return false;
-    }
+	// Called repeatedly when this Command is scheduled to run
+	protected void execute() {
+	}
 
-    // Called once after isFinished returns true
-    protected void end() {
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	protected boolean isFinished() {
+		return Robot.drivetrain.motionProfileIsDone();
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    protected void interrupted() {
-    }
+	// Called once after isFinished returns true
+	protected void end() {
+		Robot.drivetrain.disableMotionProfile();
+	}
+
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	protected void interrupted() {
+		end();
+	}
 }
