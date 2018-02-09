48b6d6e46e60f8edcc7c1ae9517627792e9d868b
kaitoa19 <kaito_arai@asl.org>
2018-02-09 11:08:53 +0000

    Revert "Revert "Added stop elevator""
    This reverts commit af910f27eca69745f1f7ea59518c2836c8e9c47e.

--- Griffins 1884/src/org/usfirst/frc/team1884/robot/commands/ElevatorLowCommand.java
+++ Griffins 1884/src/org/usfirst/frc/team1884/robot/commands/ElevatorLowCommand.java
@@ -32,6 +32,7 @@ public class ElevatorLowCommand extends Command {
 
 	// Called once after isFinished returns true
 	protected void end() {
+		Robot.elevator.stopElevator();
 	}
 
 	// Called when another command which requires one or more of the same
