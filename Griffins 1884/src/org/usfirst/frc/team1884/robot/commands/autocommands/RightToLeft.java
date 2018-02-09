e734b145bf0737eca8d8b9960eaf8429c450467a
Axel <axel_henrikson@asl.org>
2018-02-09 10:18:04 +0000

    Removed unnecessary comments

--- Griffins 1884/src/org/usfirst/frc/team1884/robot/commands/autocommands/RightToLeft.java
+++ Griffins 1884/src/org/usfirst/frc/team1884/robot/commands/autocommands/RightToLeft.java
@@ -17,40 +17,20 @@ import edu.wpi.first.wpilibj.command.CommandGroup;
  */
 public class RightToLeft extends CommandGroup {
 
-    public RightToLeft() {
-    	addSequential(new MotionProfile3B());
-    	addSequential(new MotionProfileBD());
-    	//Add command to put on switch here
-    	addSequential(new ElevatorTopCommand());
-    	addSequential(new ElevatorClawCommand());
-    	addSequential(new ElevatorLowCommand());
-    	addSequential(new MotionProfileRot180());
-    	addSequential(new MotionProfileDF());
-    	//Add command to pick up box
-    	addSequential(new IntakeInCommand());
-    	addSequential(new ElevatorClawCommand());
-    	addSequential(new MotionProfileRot180());
-    	addSequential(new MotionProfileFD());
-    	//Add command to put on switch here
-    	addSequential(new ElevatorTopCommand());
-    	addSequential(new ElevatorClawCommand());
-    	
-    	
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
+	public RightToLeft() {
+		addSequential(new MotionProfile3B());
+		addSequential(new MotionProfileBD());
+		addSequential(new ElevatorTopCommand());
+		addSequential(new ElevatorClawCommand());
+		addSequential(new ElevatorLowCommand());
+		addSequential(new MotionProfileRot180());
+		addSequential(new MotionProfileDF());
+		addSequential(new IntakeInCommand());
+		addSequential(new ElevatorClawCommand());
+		addSequential(new MotionProfileRot180());
+		addSequential(new MotionProfileFD());
+		addSequential(new ElevatorTopCommand());
+		addSequential(new ElevatorClawCommand());
 
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
-    }
+	}
 }
--- Griffins 1884/src/org/usfirst/frc/team1884/robot/commands/autocommands/RightToRight.java
+++ Griffins 1884/src/org/usfirst/frc/team1884/robot/commands/autocommands/RightToRight.java
@@ -20,36 +20,17 @@ public class RightToRight extends CommandGroup {
     public RightToRight() {
     	addSequential(new MotionProfile3B());
     	addSequential(new MotionProfileBD());
-    	//Add command to put on switch here
     	addSequential(new ElevatorTopCommand());
     	addSequential(new ElevatorClawCommand());
     	addSequential(new ElevatorLowCommand());
     	addSequential(new MotionProfileRot180());
     	addSequential(new MotionProfileDF());
-    	//Add command to pick up box
     	addSequential(new IntakeInCommand());
     	addSequential(new ElevatorClawCommand());
     	addSequential(new MotionProfileRot180());
     	addSequential(new MotionProfileFD());
-    	//Add command to put on switch here
     	addSequential(new ElevatorTopCommand());
     	addSequential(new ElevatorClawCommand());
     	
-        // Add Commands here:
-        // e.g. addSequential(new Command1());
-        //      addSequential(new Command2());
-        // these will run in order.
-
-        // To run multiple commands at the same time,
-        // use addParallel()
-        // e.g. addParallel(new Command1());
-        //      addSequential(new Command2());
-        // Command1 and Command2 will run in parallel.
-
-        // A command group will require all of the subsystems that each member
-        // would require.
-        // e.g. if Command1 requires chassis, and Command2 requires arm,
-        // a CommandGroup containing them would require both the chassis and the
-        // arm.
     }
 }
