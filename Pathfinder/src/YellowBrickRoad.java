cc2774ae733a0cb7c00a517712b50588e48e382f
Kaito Arai <kaito_arai@asl.org>
2018-02-12 16:44:42 +0000

    Made new trajectories and others redundant

--- Pathfinder/src/YellowBrickRoad.java
+++ Pathfinder/src/YellowBrickRoad.java
@@ -8,8 +8,8 @@ import jaci.pathfinder.modifiers.TankModifier;
 public class YellowBrickRoad {
 
 	final static double MAX_VELOCITY = 3.841;
-	final static double MAX_ACCELERATION = 114.84;
-	final static double MAX_JERK = 11981.157;
+	final static double MAX_ACCELERATION = 6.215;
+	final static double MAX_JERK = 10.055;
 
 	public static void main(String[] args) {
 
@@ -30,33 +30,37 @@ public class YellowBrickRoad {
 		File rightFile1A = new File("rightFile1A.csv");
 		Pathfinder.writeToCSV(rightFile1A, right1A);
 
-		Waypoint[] points2A = new Waypoint[] { new Waypoint(0.495, 3.980, 0), new Waypoint(4.267, 7.028, 0) };
-
-		Trajectory trajectory2A = Pathfinder.generate(points2A, config);
-
-		TankModifier modifier2A = new TankModifier(trajectory2A).modify(0.64);
-		Trajectory left2A = modifier2A.getLeftTrajectory();
-		Trajectory right2A = modifier2A.getRightTrajectory();
-
-		File leftFile2A = new File("leftFile2A.csv");
-		Pathfinder.writeToCSV(leftFile2A, left2A);
-
-		File rightFile2A = new File("rightFile2A.csv");
-		Pathfinder.writeToCSV(rightFile2A, right2A);
-
-		Waypoint[] points2B = new Waypoint[] { new Waypoint(0.495, 3.980, 0), new Waypoint(4.267, 1.186, 0) };
-
-		Trajectory trajectory2B = Pathfinder.generate(points2B, config);
-
-		TankModifier modifier2B = new TankModifier(trajectory2B).modify(0.64);
-		Trajectory left2B = modifier2B.getLeftTrajectory();
-		Trajectory right2B = modifier2B.getRightTrajectory();
-
-		File leftFile2B = new File("leftFile2B.csv");
-		Pathfinder.writeToCSV(leftFile2B, left2B);
-
-		File rightFile2B = new File("rightFile2B.csv");
-		Pathfinder.writeToCSV(rightFile2B, right2B);
+		/*
+		 * Waypoint[] points2A = new Waypoint[] { new Waypoint(0.495, 3.980, 0), new
+		 * Waypoint(4.267, 7.028, 0) };
+		 * 
+		 * Trajectory trajectory2A = Pathfinder.generate(points2A, config);
+		 * 
+		 * TankModifier modifier2A = new TankModifier(trajectory2A).modify(0.64);
+		 * Trajectory left2A = modifier2A.getLeftTrajectory(); Trajectory right2A =
+		 * modifier2A.getRightTrajectory();
+		 * 
+		 * File leftFile2A = new File("leftFile2A.csv");
+		 * Pathfinder.writeToCSV(leftFile2A, left2A);
+		 * 
+		 * File rightFile2A = new File("rightFile2A.csv");
+		 * Pathfinder.writeToCSV(rightFile2A, right2A);
+		 * 
+		 * Waypoint[] points2B = new Waypoint[] { new Waypoint(0.495, 3.980, 0), new
+		 * Waypoint(4.267, 1.186, 0) };
+		 * 
+		 * Trajectory trajectory2B = Pathfinder.generate(points2B, config);
+		 * 
+		 * TankModifier modifier2B = new TankModifier(trajectory2B).modify(0.64);
+		 * Trajectory left2B = modifier2B.getLeftTrajectory(); Trajectory right2B =
+		 * modifier2B.getRightTrajectory();
+		 * 
+		 * File leftFile2B = new File("leftFile2B.csv");
+		 * Pathfinder.writeToCSV(leftFile2B, left2B);
+		 * 
+		 * File rightFile2B = new File("rightFile2B.csv");
+		 * Pathfinder.writeToCSV(rightFile2B, right2B);
+		 */
 
 		Waypoint[] points3B = new Waypoint[] { new Waypoint(0.495, 1.186, 0), new Waypoint(4.267, 1.186, 0) };
 
@@ -72,61 +76,67 @@ public class YellowBrickRoad {
 		File rightFile3B = new File("rightFile3B.csv");
 		Pathfinder.writeToCSV(rightFile3B, right3B);
 
-		Waypoint[] pointsAC = new Waypoint[] { new Waypoint(4.267, 7.028, 0), new Waypoint(7.116, 6.226, 0) };
-
-		Trajectory trajectoryAC = Pathfinder.generate(pointsAC, config);
-
-		TankModifier modifierAC = new TankModifier(trajectoryAC).modify(0.64);
-		Trajectory leftAC = modifierAC.getLeftTrajectory();
-		Trajectory rightAC = modifierAC.getRightTrajectory();
-
-		File leftFileAC = new File("leftFileAC.csv");
-		Pathfinder.writeToCSV(leftFileAC, leftAC);
-
-		File rightFileAC = new File("rightFileAC.csv");
-		Pathfinder.writeToCSV(rightFileAC, rightAC);
-
-		Waypoint[] pointsAD = new Waypoint[] { new Waypoint(4.267, 7.028, 0), new Waypoint(7.116, 1.988, 0) };
-
-		Trajectory trajectoryAD = Pathfinder.generate(pointsAD, config);
-
-		TankModifier modifierAD = new TankModifier(trajectoryAD).modify(0.64);
-		Trajectory leftAD = modifierAD.getLeftTrajectory();
-		Trajectory rightAD = modifierAD.getRightTrajectory();
-
-		File leftFileAD = new File("leftFileAD.csv");
-		Pathfinder.writeToCSV(leftFileAD, leftAD);
-
-		File rightFileAD = new File("rightFileAD.csv");
-		Pathfinder.writeToCSV(rightFileAD, rightAD);
-
-		Waypoint[] pointsBC = new Waypoint[] { new Waypoint(4.267, 1.186, 0), new Waypoint(7.116, 6.226, 0) };
-
-		Trajectory trajectoryBC = Pathfinder.generate(pointsBC, config);
-
-		TankModifier modifierBC = new TankModifier(trajectoryBC).modify(0.64);
-		Trajectory leftBC = modifierBC.getLeftTrajectory();
-		Trajectory rightBC = modifierBC.getRightTrajectory();
-
-		File leftFileBC = new File("leftFileBC.csv");
-		Pathfinder.writeToCSV(leftFileBC, leftBC);
-
-		File rightFileBC = new File("rightFileBC.csv");
-		Pathfinder.writeToCSV(rightFileBC, rightBC);
-
-		Waypoint[] pointsBD = new Waypoint[] { new Waypoint(4.267, 1.186, 0), new Waypoint(7.116, 1.988, 0) };
-
-		Trajectory trajectoryBD = Pathfinder.generate(pointsBD, config);
-
-		TankModifier modifierBD = new TankModifier(trajectoryBD).modify(0.64);
-		Trajectory leftBD = modifierBD.getLeftTrajectory();
-		Trajectory rightBD = modifierBD.getRightTrajectory();
-
-		File leftFileBD = new File("leftFileBD.csv");
-		Pathfinder.writeToCSV(leftFileBD, leftBD);
-
-		File rightFileBD = new File("rightFileBD.csv");
-		Pathfinder.writeToCSV(rightFileBD, rightBD);
+		/*
+		 * Waypoint[] pointsAC = new Waypoint[] { new Waypoint(4.267, 7.028, 0), new
+		 * Waypoint(7.116, 6.226, 0) };
+		 * 
+		 * Trajectory trajectoryAC = Pathfinder.generate(pointsAC, config);
+		 * 
+		 * TankModifier modifierAC = new TankModifier(trajectoryAC).modify(0.64);
+		 * Trajectory leftAC = modifierAC.getLeftTrajectory(); Trajectory rightAC =
+		 * modifierAC.getRightTrajectory();
+		 * 
+		 * File leftFileAC = new File("leftFileAC.csv");
+		 * Pathfinder.writeToCSV(leftFileAC, leftAC);
+		 * 
+		 * File rightFileAC = new File("rightFileAC.csv");
+		 * Pathfinder.writeToCSV(rightFileAC, rightAC);
+		 * 
+		 * Waypoint[] pointsAD = new Waypoint[] { new Waypoint(4.267, 7.028, 0), new
+		 * Waypoint(7.116, 1.988, 0) };
+		 * 
+		 * Trajectory trajectoryAD = Pathfinder.generate(pointsAD, config);
+		 * 
+		 * TankModifier modifierAD = new TankModifier(trajectoryAD).modify(0.64);
+		 * Trajectory leftAD = modifierAD.getLeftTrajectory(); Trajectory rightAD =
+		 * modifierAD.getRightTrajectory();
+		 * 
+		 * File leftFileAD = new File("leftFileAD.csv");
+		 * Pathfinder.writeToCSV(leftFileAD, leftAD);
+		 * 
+		 * File rightFileAD = new File("rightFileAD.csv");
+		 * Pathfinder.writeToCSV(rightFileAD, rightAD);
+		 * 
+		 * Waypoint[] pointsBC = new Waypoint[] { new Waypoint(4.267, 1.186, 0), new
+		 * Waypoint(7.116, 6.226, 0) };
+		 * 
+		 * Trajectory trajectoryBC = Pathfinder.generate(pointsBC, config);
+		 * 
+		 * TankModifier modifierBC = new TankModifier(trajectoryBC).modify(0.64);
+		 * Trajectory leftBC = modifierBC.getLeftTrajectory(); Trajectory rightBC =
+		 * modifierBC.getRightTrajectory();
+		 * 
+		 * File leftFileBC = new File("leftFileBC.csv");
+		 * Pathfinder.writeToCSV(leftFileBC, leftBC);
+		 * 
+		 * File rightFileBC = new File("rightFileBC.csv");
+		 * Pathfinder.writeToCSV(rightFileBC, rightBC);
+		 * 
+		 * Waypoint[] pointsBD = new Waypoint[] { new Waypoint(4.267, 1.186, 0), new
+		 * Waypoint(7.116, 1.988, 0) };
+		 * 
+		 * Trajectory trajectoryBD = Pathfinder.generate(pointsBD, config);
+		 * 
+		 * TankModifier modifierBD = new TankModifier(trajectoryBD).modify(0.64);
+		 * Trajectory leftBD = modifierBD.getLeftTrajectory(); Trajectory rightBD =
+		 * modifierBD.getRightTrajectory();
+		 * 
+		 * File leftFileBD = new File("leftFileBD.csv");
+		 * Pathfinder.writeToCSV(leftFileBD, leftBD);
+		 * 
+		 * File rightFileBD = new File("rightFileBD.csv");
+		 * Pathfinder.writeToCSV(rightFileBD, rightBD);
+		 */
 
 		Waypoint[] pointsCE = new Waypoint[] { new Waypoint(7.116, 6.226, Math.PI),
 				new Waypoint(5.803, 5.883, Math.PI) };
